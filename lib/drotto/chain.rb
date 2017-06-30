beaba073b860f9f7eec54320f8a13be042eaa414
Anthony Martin <github@martin-studio.com>
2017-06-30 16:13:41 -0700

    bid stacking

--- lib/drotto/chain.rb
+++ lib/drotto/chain.rb
@@ -77,73 +77,143 @@ module DrOtto
     end
     
     def vote(bids)
-      total = bids.map { |bid| bid[:amount].split(' ').first.to_i }.reduce(0, :+)
+      # First, we need a total of all bids for this batch.  This will be used to
+      # figure out how much each bid is allocated.
+      total = bids.map { |bid| bid[:amount].split(' ').first.to_f }.reduce(0, :+)
+      
+      # Vote stacking is where multiple bids are created for the same post.  Any
+      # number of transfers from any number of accounts can bid on the same
+      # post.
+      
+      stacked_bids = {}
       
       bids.each do |bid|
-        from = bid[:from]
-        amount = bid[:amount].split(' ').first.to_i
-        author = bid[:author]
-        permlink = bid[:permlink]
-        parent_permlink = bid[:parent_permlink]
-        parent_author = bid[:parent_author]
-        timestamp = bid[:permlink]
-        coeff = (amount.to_f / total.to_f)
+        stacked_bids[bid[:author] => bid[:permlink]] ||= {}
+        stacked_bid = stacked_bids[bid[:author] => bid[:permlink]]
         
-        debug "Total: #{total}; amount: #{amount}"
-        debug "Voting for #{author}/#{permlink} with a coefficnent of #{coeff}."
-        
-        vote = {
-          type: :vote,
-          voter: account_name,
-          author: author,
-          permlink: permlink,
-          weight: (weight = batch_vote_weight * coeff).to_i
-        }
-        
-        merge_options = {
-          markup: :html,
-          content_type: parent_author == '' ? 'post' : 'comment',
-          vote_weight_percent: ("%.2f" % (weight / 100)),
-          vote_type: weight > 0 ? 'upvote' : 'downvote',
-          account_name: account_name,
-          from: from
-        }
-        
-        comment = {
-          type: :comment,
-          parent_permlink: permlink,
-          author: account_name,
-          permlink: "re-#{author.gsub(/[^a-z0-9\-]+/, '-')}-#{permlink}-#{Time.now.utc.strftime('%Y%m%dt%H%M%S%Lz')}", # e.g.: 20170225t235138025z
-          title: '',
-          body: merge(merge_options),
-          json_metadata: "{\"tags\":[\"#{parent_permlink}\"],\"app\":\"#{DrOtto::AGENT_ID}\"}",
-          parent_author: author
-        }
-        
-        tx = Radiator::Transaction.new(chain_options.merge(wif: posting_wif))
-        tx.operations << vote
-        tx.operations << comment
-        
-        response = nil
-        
-        begin
-          response = tx.process(true)
-        rescue => e
-          info "Unable to vote and comment, retrying with just vote: #{e}"
+        if stacked_bid.empty?
+          stacked_bid[:trx_id] = bid[:trx_id]
+          stacked_bid[:from] = [bid[:from]]
+          stacked_bid[:amount] = [bid[:amount]]
+          stacked_bid[:author] = bid[:author]
+          stacked_bid[:permlink] = bid[:permlink]
+          stacked_bid[:parent_permlink] = bid[:parent_permlink]
+          stacked_bid[:parent_author] = bid[:parent_author]
+          stacked_bid[:permlink] = bid[:permlink]
+          stacked_bid[:timestamp] = bid[:timestamp]
+        else
+          stacked_bid[:from] << bid[:from]
+          stacked_bid[:amount] << bid[:amount]
         end
-        
-        if response.nil? || !!response.error
-          info "Retrying with just vote: #{response}"
-          tx.operations = [vote]
-          
-          begin
-            response = tx.process(true)
-          rescue => e
-            info "Unable to vote: #{e}"
+      end
+      
+      bids = stacked_bids.values
+      
+      bids.each do |bid|
+        # We are using asynchronous voting because sometimes the blockchain
+        # rejects votes that happen too quickly.
+        bid[:thread] = Thread.new do
+          loop do
+            from = bid[:from]
+            amount = bid[:amount].map{ |a| a.split(' ').first.to_f }.reduce(0, :+)
+            author = bid[:author]
+            permlink = bid[:permlink]
+            parent_permlink = bid[:parent_permlink]
+            parent_author = bid[:parent_author]
+            timestamp = bid[:timestamp]
+            coeff = (amount.to_f / total.to_f)
+            
+            debug "Total: #{total}; amount: #{amount}"
+            debug "Voting for #{author}/#{permlink} with a coefficnent of #{coeff}."
+            
+            vote = {
+              type: :vote,
+              voter: account_name,
+              author: author,
+              permlink: permlink,
+              weight: (weight = batch_vote_weight * coeff).to_i
+            }
+            
+            merge_options = {
+              markup: :html,
+              content_type: parent_author == '' ? 'post' : 'comment',
+              vote_weight_percent: ("%.2f" % (weight / 100)),
+              vote_type: weight > 0 ? 'upvote' : 'downvote',
+              account_name: account_name,
+              from: from
+            }
+            
+            comment = {
+              type: :comment,
+              parent_permlink: permlink,
+              author: account_name,
+              permlink: "re-#{author.gsub(/[^a-z0-9\-]+/, '-')}-#{permlink}-#{Time.now.utc.strftime('%Y%m%dt%H%M%S%Lz')}", # e.g.: 20170225t235138025z
+              title: '',
+              body: merge(merge_options),
+              json_metadata: "{\"tags\":[\"#{parent_permlink}\"],\"app\":\"#{DrOtto::AGENT_ID}\"}",
+              parent_author: author
+            }
+            
+            tx = Radiator::Transaction.new(chain_options.merge(wif: posting_wif))
+            tx.operations << vote
+            tx.operations << comment
+            
+            response = nil
+            
+            begin
+              response = tx.process(true)
+            rescue => e
+              warning "Unable to vote and comment, retrying with just vote: #{e}", e
+            end
+            
+            if !!response && !!response.error
+              message = response.error.message
+              if message.to_s =~ /You have already voted in a similar way./
+                error "Failed vote: duplicate vote."
+                break
+              elsif message.to_s =~ /You may only comment once every 20 seconds./
+                warning "Retrying vote/comment: commenting too quickly."
+                redo
+              elsif message.to_s =~ /Can only vote once every 3 seconds./
+                warning "Retrying vote: voting too quickly."
+                redo
+              elsif message.to_s =~ /Voting weight is too small, please accumulate more voting power or steem power./
+                error "Failed vote: voting weight too small"
+                break
+              elsif message.to_s =~ /Vote weight cannot be 0/
+                error "Failed vote: vote weight cannot be zero."
+                break
+              elsif message.to_s =~ /STEEMIT_UPVOTE_LOCKOUT_HF17/
+                error "Failed vote: upvote lockout (last twelve hours before payout)"
+                break
+              elsif message.to_s =~ /missing required posting authority/
+                error "Failed vote: Check posting key."
+                break
+              elsif message.to_s =~ /unknown key/
+                error "Failed vote: unknown key (testing?)"
+                break
+              elsif message.to_s =~ /signature is not canonical/
+                warning "Retrying vote/comment: signature was not canonical (bug in Radiator?)"
+                redo
+              end
+            end
+
+            if response.nil? || !!response.error
+              warning "Unknown problem while voting.  Retrying with just vote: #{response}"
+              tx.operations = [vote]
+              
+              begin
+                response = tx.process(true)
+              rescue => e
+                error "Unable to vote: #{e}", e
+              end
+            end
+            
+            info response unless response.nil?
+            
+            break
           end
         end
-        
-        info response unless response.nil?
       end
     end
   end
